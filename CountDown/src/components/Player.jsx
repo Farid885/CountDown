fdc6947dcf1bd9387a449bb83da4cde150b06ed1
Farid885 <ferid14zamanli@gmail.com>
2024-02-10 15:57:27 +0400

    first steps

--- CountDown/src/components/Player.jsx
+++ CountDown/src/components/Player.jsx
@@ -1,22 +1,29 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 export default function Player() {
   const [enteredPlayer, setEnteredPlayer] = useState(null);
-  const [submitted, setSubmitted] = useState(false);
-  function handleChange(event) {
-    setSubmitted(false);
-    setEnteredPlayer(event.target.value);
-  }
-  console.log(enteredPlayer);
-  console.log(submitted);
+  // const [submitted, setSubmitted] = useState(false);
+
+  const playerName = useRef();
+  console.log(playerName);
+
+  // function handleChange(event) {
+  //   setSubmitted(false);
+  //   setEnteredPlayer(event.target.value);
+  // }
 
   function handleClick() {
-    setSubmitted(true);
+    setEnteredPlayer(playerName.current.value);
+    if (!playerName.current.value ) {
+      return "";
+    }
   }
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredPlayer : "unknown entity"}</h2>
+      <h2>Welcome {enteredPlayer ? enteredPlayer : "unknown entity"}</h2>
+      {/* <h2>Welcome {enteredPlayer ??   "unknown entity"}</h2> */}
+      {/* line 21 and 22 is same syntax  */}
       <p>
-        <input onChange={handleChange} value={enteredPlayer} type="text" />
+        <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
